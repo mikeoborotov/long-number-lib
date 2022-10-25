af8fae5a0e8500e9180d25c9bd51e1019e09d713
OborotovMikhail <oborotoffm@yandex.ru>
2022-10-25 19:56:12 +0300

    added library file and testing code file

---
+++ include/LongNumberLib.h
@@ -0,0 +1,15 @@
+// LongNumberLib is a library for long number arithmetics in C++
+
+#pragma once
+
+#include <iostream>
+
+// A class for arbitrary length integers
+class LongInt {
+private:
+
+
+public:
+
+
+};
---
+++ source/testing.cpp
@@ -0,0 +1,11 @@
+// Testing the LongNumberLib.h
+
+#include <iostream>
+
+#include "../include/LongNumberLib.h"
+
+int main(int argc, char *argv[]) {
+	std::cout << "Running tests" << "\n";
+
+	return 0;
+}
